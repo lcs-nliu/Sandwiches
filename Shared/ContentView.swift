717c907ee8db0221e0c8f6b51900f5c4c15dbb1a
Nicole Liu <nicoleliu_@outlook.com>
2021-01-21 01:11:52 -0500

    Added editing options to app through a sandwich store

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,48 +8,82 @@
 import SwiftUI
 
 struct ContentView: View {
-    var sandwiches: [Sandwich] = []
+    @ObservedObject var store: SandwichStore
     
     var body: some View {
         NavigationView {
             List{
-                ForEach (sandwiches) { sandwich in
+                ForEach (store.sandwiches) { sandwich in
                     SandwichCell(sandwich: sandwich)
                 }
+                
+                .onMove(perform: moveSandwiches)
+                .onDelete(perform: deleteSandwiches)
+                
                 HStack {
-                    Text("\(sandwiches.count)Sandwiches")
+                    Text("\(store.sandwiches.count) Sandwiches")
                         .foregroundColor(.secondary)
                     Spacer()
                 }
             }
-                .navigationTitle("Sandwiches")
+            .navigationTitle("Sandwiches")
+            .toolbar {
+                #if os(iOS)
+                EditButton()
+                #endif
+                Button("Add", action: makeSandwich)
             }
             
+            Text("Select a sandwich.")
+                .font(.largeTitle)
+        }
+        
+    }
+    
+    func makeSandwich() {
+        withAnimation {
+            store.sandwiches.append(Sandwich(name: "Patty melt", ingredientCount: 3))
         }
     }
     
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView(sandwiches: testData)
+    func moveSandwiches(from: IndexSet, to: Int){
+        withAnimation {
+            store.sandwiches.move(fromOffsets: from, toOffset: to)
         }
     }
     
     
-    struct SandwichCell: View {
-        var sandwich: Sandwich
-        var body: some View {
-            NavigationLink(destination: SandwichDetail(sandwich:sandwich)) {
-                Image(sandwich.thumbnailName)
-                    .cornerRadius(8)
+    func deleteSandwiches(offsets: IndexSet){
+        withAnimation {
+            store.sandwiches.remove(atOffsets: offsets)
+        }
+    }
+    
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(store: testStore)
+    }
+}
+
+
+struct SandwichCell: View {
+    var sandwich: Sandwich
+    var body: some View {
+        NavigationLink(destination: SandwichDetail(sandwich:sandwich)) {
+            Image(sandwich.thumbnailName)
+                .cornerRadius(8)
+            
+            VStack(alignment: .leading) {
                 
-                VStack(alignment: .leading) {
-                    
-                    Text(sandwich.name)
-                    Text("\(sandwich.ingredientCount) ingredients")
-                        .font(.subheadline)
-                        .foregroundColor(.secondary)
-                    
-                }
+                Text(sandwich.name)
+                Text("\(sandwich.ingredientCount) ingredients")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                
             }
         }
     }
+}
